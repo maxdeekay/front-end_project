34e1a8005f96fea048e2d59410f1d29a69baeea0
Max <max.assermark@gmail.com>
2024-03-04 18:35:05 +0100

    testing API

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+window.onload = async () => {
+    const url = "https://api.skolverket.se/skolenhetsregistret/v1/skolenhet/33139103";
+    const response = await fetch(url);
+
+    if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+    } else {
+        console.log("ERROR")
+    }
+}
