0dbdc66a1e10bb0b1ff5dea9a1ca4f51f20f4a1a
Max <max.assermark@gmail.com>
2024-03-04 21:42:10 +0100

    added netlify proxy

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 "use strict";
 
 window.onload = async () => {
-    /* const url = "https://api.edamam.com/search?q=chicken&app_id=5bcb567c&app_key=8ef1eb8193f7901d7157808ff94bda29";
+    const url = "/skolenhetsregistret/v1/skolenhet/33139103";
     const response = await fetch(url);
 
     if (response.ok) {
@@ -9,22 +9,8 @@ window.onload = async () => {
         console.log(data);
     } else {
         console.log("ERROR")
-    } */
-
-    const url = "/skolenhetsregistret/v1/skolenhet/33139103";
-    const response = await fetch(url);
-
-    if (response.ok) {
-        const data = await response.json();
-        console.log(data);
     }
 }
 
-/* function fetchBjorkvik() {
-    const res = fetch("/skolenhetsregistret/v1/skolenhet/33139103")
-      .then((response) => response.json())
-      .then((data) => {
-        document.getElementById("output").innerHTML =
-          data["SkolenhetInfo"]["Namn"];
-      });
-  } */
+
+// weather api key: 2c871b7f911399ff66f6268c00f1f52a
