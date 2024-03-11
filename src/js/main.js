449ec0b64d84d8f2906f8473cf1a1ea84fa95bcd
Max <max.assermark@gmail.com>
2024-03-12 00:51:22 +0100

    updated styling and arrow keys functionality

--- src/js/main.js
+++ src/js/main.js
@@ -21,13 +21,24 @@ window.onload = async () => {
 
         input.addEventListener("input", debounce(() => {
             searchContainer.classList.add("at-top");
-            document.getElementById("intro-text").classList.add("fade-out");
 
             search(input.value.toLowerCase(), regionsData, schoolsData);
         }));
 
+        document.addEventListener("keydown", (e) => {
+            if (document.querySelectorAll(".suggestion").length != 0) {
+                if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+                    e.preventDefault();
+                    moveSelection(e.key);
+                }
+
+                if (e.key === "Enter") select();
+                if (e.key === "Backspace" && input.value === "") removeTag();
+            };
+        });
+
     } else {
-        console.log("ERRORS");
+        console.log("ERROR");
     }
 
     document.addEventListener("keydown", (e) => {
@@ -38,7 +49,7 @@ window.onload = async () => {
         }
     });
 
-    initializeTag();
+    document.getElementById("remove-tag").onclick = () => removeTag();
     initializeMap();
 }
 
@@ -49,6 +60,45 @@ const debounce = (fn, delay = 20) => {
         timerId = setTimeout(() => fn(...args), delay); 
     }; 
 };
+
+function moveSelection(key) {
+    const suggestions = document.querySelectorAll("[index]");
+    const option = document.querySelector(".selected");
+    const operator = (key === "ArrowUp") ? -1 : 1;
+
+    if (option) {
+        option.classList.remove("selected");
+        const max = suggestions[suggestions.length-1].getAttribute("index");
+        let query = "";
+
+        if (option.getAttribute("index") === "0" && key === "ArrowUp") {
+            query = `[index="${max}"]`;
+        } else if (option.getAttribute("index") === max && key === "ArrowDown") {
+            query = `[index="0"]`;
+        } else {
+            query = `[index="${parseInt(option.getAttribute("index"))+operator}"]`;
+        }
+
+        const newSuggestion = document.querySelector(query);
+        newSuggestion.classList.add("selected");
+
+        document.querySelector(".selected").scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'nearest' });
+    }
+    
+    if (!option && key === "ArrowDown") document.querySelectorAll(".suggestion")[0].classList.add("selected");
+}
+
+function select() {
+    const selection = document.querySelector(".selected");
+
+    if (selection) {
+        if (!selection.getAttribute("name")) {
+            showSchool(selection.getAttribute("code"));
+        } else {
+            addSearchTag(selection.getAttribute("name"), selection.getAttribute("code"));
+        }
+    }
+}
 
 function search(input, regions, schools) {
     if (globalState.tag) {
@@ -64,7 +114,7 @@ function search(input, regions, schools) {
             return school.Skolenhetsnamn.toLowerCase().includes(input);
         });
     
-        suggest(schoolsResult.slice(0, 5), regionsResult.slice(0, 5));
+        suggest(schoolsResult.slice(0, 50), regionsResult.slice(0, 50));
     }
 }
 
@@ -83,22 +133,7 @@ function suggest(schools, regions = null) {
         container.appendChild(div);
     }
     
-    schools.forEach((school) => {
-        const div = document.createElement("div");
-        const name = document.createElement("p");
-        const nameText = document.createTextNode(school.Skolenhetsnamn);
-        const type = document.createElement("p");
-        type.innerHTML = "Skola";
-
-        div.classList.add("suggestion");
-        type.classList.add("type-text");
-
-        name.appendChild(nameText);
-        div.appendChild(name);
-        div.appendChild(type);
-        div.onclick = () => showSchool(school.Skolenhetskod);
-        container.appendChild(div);
-    });
+    let index = 0;
 
     if (regions) {
         regions.forEach((region) => {
@@ -108,22 +143,51 @@ function suggest(schools, regions = null) {
             const type = document.createElement("p");
             type.innerHTML = "Kommun";
     
+            div.setAttribute("index", index);
+            div.setAttribute("name", region.Namn);
+            div.setAttribute("code", region.Kommunkod);
             div.classList.add("suggestion");
             type.classList.add("type-text");
     
             name.appendChild(nameText);
             div.appendChild(name);
             div.appendChild(type);
-            div.onclick = () => {
-                addSearchTag(region.Namn, region.Kommunkod);
-                const input = document.getElementById("input");
-                input.value = "";
-                input.focus();
-                container.style.display = "none";
-            };
+            div.addEventListener("mouseover", () => mouseEvent(div, "add"));
+            div.addEventListener("mouseout", () => mouseEvent(div, "remove"));
+            div.onclick = () => addSearchTag(region.Namn, region.Kommunkod);
             container.appendChild(div);
+            index++;
         });
     }
+
+    schools.forEach((school) => {
+        const div = document.createElement("div");
+        const name = document.createElement("p");
+        const nameText = document.createTextNode(school.Skolenhetsnamn);
+        const type = document.createElement("p");
+        type.innerHTML = "Skola";
+
+        div.setAttribute("index", index);
+        div.setAttribute("code", school.Skolenhetskod);
+        div.classList.add("suggestion");
+        type.classList.add("type-text");
+
+        name.appendChild(nameText);
+        div.appendChild(name);
+        div.appendChild(type);
+        div.addEventListener("mouseover", () => mouseEvent(div, "add"));
+        div.addEventListener("mouseout", () => mouseEvent(div, "remove"));
+        div.onclick = () => showSchool(school.Skolenhetskod);
+        container.appendChild(div);
+        index++;
+    });
+}
+
+function mouseEvent(element, option) {
+    const prevSelect = document.querySelector(".selected");
+    if (prevSelect) prevSelect.classList.remove("selected");
+
+    option === "add" ? element.classList.add("selected") : element.classList.remove("selected");
 }
 
 async function showSchool(code) {
@@ -131,13 +195,13 @@ async function showSchool(code) {
     const school = await getSchool(code);
     const container = document.getElementById("school-container");
     container.style.display = "flex";
-    console.log(school);
+    
+    document.getElementById("input").value = school.SkolenhetInfo.Namn;
 
     document.getElementById("school-name").innerHTML = school.SkolenhetInfo.Namn;
+    document.getElementById("principal-name").innerHTML = `Rektor: ${school.SkolenhetInfo.Rektorsnamn}`;
     document.getElementById("street").innerHTML = school.SkolenhetInfo.Besoksadress.Adress;
-    document.getElementById("zip-code").innerHTML = school.SkolenhetInfo.Besoksadress.Postnr;
-    document.getElementById("city").innerHTML = school.SkolenhetInfo.Besoksadress.Ort;
-    document.getElementById("principal-name").innerHTML = school.SkolenhetInfo.Rektorsnamn;
+    document.getElementById("zip-city").innerHTML = `${school.SkolenhetInfo.Besoksadress.Postnr} ${school.SkolenhetInfo.Besoksadress.Ort}`;
 
     const ul = document.getElementById("school-forms");
     ul.innerHTML = "";
@@ -175,6 +239,13 @@ function addSearchTag(name, code) {
     tag.style.display = "flex";
     tagName.innerHTML = name;
     globalState.tag = code;
+
+    const input = document.getElementById("input");
+    const container = document.getElementById("suggestion-container");
+    input.value = "";
+    input.placeholder = "Skriv namnet på en skola... ";
+    input.focus();
+    container.style.display = "none";
 }
 
 function resetPage() {
@@ -186,13 +257,14 @@ function resetPage() {
     suggestions.innerHTML = "";
 }
 
-function initializeTag() {
+function removeTag() {
     const tag = document.getElementById("search-tag");
-    document.getElementById("remove-tag").onclick = () => {
-        tag.style.display = "none";
-        globalState.tag = null;
-        document.getElementById("input").focus();
-    }
+    tag.style.display = "none";
+    globalState.tag = null;
+    
+    const input  = document.getElementById("input");
+    input.placeholder = "Skriv namnet på en skola eller kommun..."
+    input.focus();
 }
 
 function initializeMap() {
